259d8fa7d738db2fc7343f8a8cd1c1896ff3227b
bdisptn <budisptn13@gmail.com>
2026-09-12 13:37:44 +0700

    Tugas Jobsheet W03

---
+++ Praktikum03/TestKoperasi.java
@@ -0,0 +1,29 @@
+package Praktikum03;
+
+public class TestKoperasi {
+    public static void main(String[] args) {
+        Anggota anggota1 = new Anggota("111333444", "Donny", 5000000);
+
+        System.out.println("Nama Anggota: " + anggota1.getNama());
+        System.out.println("Limit Pinjaman: " + anggota1.getLimitPinjaman());
+
+        System.out.println("\nMeminjam uang 10.000.000...");
+        anggota1.pinjam(10000000);
+        System.out.println("Jumlah pinjaman saat ini: " + anggota1.getJumlahPinjaman());
+
+        // Pengujian 1: Mengangsur 200.000 (Kurang dari 10% dari 4.000.000, yaitu 400.000)
+        System.out.println("\nMembayar angsuran 4.000.000");
+        anggota1.pinjam(4000000);
+        System.out.println("Jumlah pinjaman saat ini: " + anggota1.getJumlahPinjaman());
+
+        // Pengujian 2: Mengangsur 1.000.000 (Memenuhi syarat minimal 10%)
+        System.out.println("\nMembayar angsuran 1.000.000");
+        anggota1.angsur(1000000);
+        System.out.println("Jumlah pinjaman saat ini: " + anggota1.getJumlahPinjaman());
+
+        // Pengujian 3: Mengangsur 3.000.000 (Memenuhi syarat minimal 10% dari sisa 3.000.000)
+        System.out.println("\nMembayar angsuran 3.000.000");
+        anggota1.angsur(3000000);
+        System.out.println("Jumlah pinjaman saat ini: " + anggota1.getJumlahPinjaman());
+    }
+}
