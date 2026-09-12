8360adec66e94b0277d383216223c07d9a096a3d
bdisptn <budisptn13@gmail.com>
2026-09-12 17:32:06 +0700

    Tugas Jobsheet W03 Modifikasi

--- Praktikum03/Anggota.java
+++ Praktikum03/Anggota.java
@@ -39,6 +39,10 @@ public class Anggota {
     }
 
     public void angsur(int nominal) {
-        this.jumlahPinjaman -= nominal; 
+        if (nominal < (0.10 * this.jumlahPinjaman)) {
+            System.out.println("Maaf, angsuran harus 10% dari jumlah pinjaman");
+        } else {
+            this.jumlahPinjaman -= nominal;
+        }
     }
 } 
